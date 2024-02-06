8384210a40acd54d816bc0c4229305bc1e30f5e1
Karem <karemdarwesh1035@gmail.com>
2024-02-06 22:56:04 +0200

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -4,31 +4,54 @@ attributes/methods for other classes"""
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     we are going to define all common attribute and methods
     those are
-    
+
     public instance attributes:-
-    
+
     id: assign a unique id by using uuid.uuid4() for basemodel
     created_at: current datetime when an instance created
     updated_at: current datetime when an instance is created and updatd
 
     public instance:-
 
-    save(self): updates the public instance attribute 
+    save(self): updates the public instance attribute
     updated_at with the current datetime
 
-    to_dict(self): returns a dictionary containing 
+    to_dict(self): returns a dictionary containing
     all keys/values of __dict__ of the instance:
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ this is constructor for id and datetime"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            """
+            Updates instance attributes based on the arguments.
+
+            Args:
+                kwargs (dict): Keyword arguments of the instance attributes.
+
+            Description:
+                - iterates over the provided keyword arguments
+                - updates instance attributes accordingly
+                - If the keyword argument is not '__class__',
+                - checks if the key is 'created_at' or 'updated_at'.
+                - converts the string to a datetime object.
+            """
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f')
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """print class name, id and dictionary"""
