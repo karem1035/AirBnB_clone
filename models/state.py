f112dc27fb4b33655d2aae6aaef1672728416d25
Michael-shambel <Michaelshambel10@gmail.com>
2024-02-08 16:13:05 -0800

    task 9

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """state of the user"""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
