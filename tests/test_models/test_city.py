2d9f52cb0eb3662028ae8bafa97381fc08fbfb35
Karem <karemdarwesh1035@gmail.com>
2024-02-12 00:23:32 +0200

    executrion permission added

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,11 +2,7 @@
 """Unittest module for the City Class."""
 
 import unittest
-from datetime import datetime
-import time
 from models.city import City
-import re
-import json
 from models.engine.file_storage import FileStorage
 import os
 from models import storage
